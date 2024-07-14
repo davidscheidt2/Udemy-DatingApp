54c0717ed7db743763e54f06a1570866a7578bcb
davidscheidtbitstop <123389048+davidscheidtbitstop@users.noreply.github.com>
2024-07-14 17:51:10 -0400

    Progress on the blazor app.  Added local storage and initial home page

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
@@ -8,8 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
-builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AccountService>();
+
 
 await builder.Build().RunAsync();
--- Client/Users/Service/AccountService.cs
+++ Client/Users/Service/AccountService.cs
@@ -1,16 +1,42 @@
 using System.Net.Http.Json;
+using Blazored.LocalStorage;
 
 namespace Client.Users.Service;
 
-public class AccountService(HttpClient client) : IAccountService
+public class AccountService(HttpClient client, ILocalStorageService localStorageService)
 {
-    public async Task<HttpResponseMessage> Login(LoginModel login)
+    public async Task Login(LoginModel login)
     {
-        return await client.PostAsJsonAsync("api/account/login", login);
+        var response = await client.PostAsJsonAsync("api/account/login", login);
+        
+        if (response.IsSuccessStatusCode)
+        {
+            var user = await response.Content.ReadFromJsonAsync<UserDto>() ?? throw new Exception("Invalid response from server");
+            await localStorageService.SetItemAsync("user", user);
+            CurrentUser = user;
+            Console.WriteLine(user.Username + " " + user.Token);
+        }
+        else
+            Console.WriteLine("Invalid login attempt");
     }
-}
 
-public interface IAccountService
-{
-    Task<HttpResponseMessage> Login(LoginModel login);
+    private UserDto? _currentUser;
+
+    public async Task Logout()
+    {
+        await localStorageService.RemoveItemAsync("user");
+        CurrentUser = null;
+    }
+
+    public event Action? OnChange;
+
+    public UserDto? CurrentUser
+    {
+        get => _currentUser;
+        set
+        {
+            _currentUser = value;
+            OnChange?.Invoke();
+        }
+    }
 }
