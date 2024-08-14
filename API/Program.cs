ffacddea4fae09761262fda58e1a01813687b049
davidscheidtbitstop <123389048+davidscheidtbitstop@users.noreply.github.com>
2024-08-14 14:41:47 -0400

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(
     "http://localhost:5000", "https://localhost:5001", "http://localhost:4200", "https://localhost:4200"));
 
