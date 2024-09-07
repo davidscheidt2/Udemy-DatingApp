1df3c39b7aae3f4cdff374578481fed408e50f7a
davidscheidt2 <123389048+davidscheidtbitstop@users.noreply.github.com>
2024-09-07 11:24:38 -0400

    End of section 17

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -30,6 +31,10 @@ public static class ApplicationServiceExtensions
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+        services.AddSignalR();
+
+        services.AddSingleton<PresenceTracker>();
 
         return services;
     }
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +17,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(
-    "http://localhost:5000", "https://localhost:5001", "http://localhost:4200", "https://localhost:4200"));
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:5000", "https://localhost:5001", "http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -33,6 +34,8 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 app.MapRazorPages();
 app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
 app.MapFallbackToFile("index.html");
 
 using var scope = app.Services.CreateScope();
@@ -44,6 +47,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
+    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
